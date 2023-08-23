279ad40df2f7e07ccd14fc9c98479f95fd9a3e48
almighty32 <126041795+almighty32@users.noreply.github.com>
2023-08-23 17:48:40 +0800

    camacho
    update

--- typescript/script.js
+++
@@ -1,7 +0,0 @@
-const input1= document.querySelector("#input1");
-const input2= document.querySelector("#input2");
-const button= document.querySelector("button");
-
-button.addEventListener("click", ()=>{
-    console.log('it was clicked')
-})
